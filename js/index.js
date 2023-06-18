7138d68a3409ba1c7a5d5fdd8b15313cfaecb095
EugeneGrishin <eugenex61@mail.ru>
2023-06-18 22:50:38 +0300

    links

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$("a.scroll-to").on("click", function(e){
-  e.preventDefault();
-  var anchor = $(this).attr('href');
-  $('html, body').stop().animate({
-      scrollTop: $(anchor).offset().top - 60
-  }, 800);
-});
